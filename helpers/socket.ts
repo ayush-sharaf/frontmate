abdbef7ecd01a690b491dc5b644577e7988cf0c4
Ayush Sharaf <ayushsharaf3010@gmail.com>
2024-11-04 23:56:13 +0530

    Update socket.ts

--- helpers/socket.ts
+++ helpers/socket.ts
@@ -1,16 +1,14 @@
 import toast from 'react-hot-toast';
-import {io, ManagerOptions, Socket, SocketOptions} from 'socket.io-client'
+import { io, ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
 
 export const initSocket = async () => {
-
-
-    const option : Partial<ManagerOptions & SocketOptions>= {
-        forceNew : true,
+    const options: Partial<ManagerOptions & SocketOptions> = {
+        forceNew: true,
         reconnectionAttempts: 10000,
-        timeout : 10000,
+        timeout: 10000,
         transports: ['websocket']
-    }
-    return io("https://frontmate.vercel.app/socket.io" , option)
+    };
+
+    const serverUrl = "https://frontmate-ejxy.vercel.app/"; 
+    return io(serverUrl, options);
 }
-
-
